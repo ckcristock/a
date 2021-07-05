61c295b75f32471a07fae40e9491d38bec6df706
Carlos Cardona <carlos.cardona@corvuslab.co>
2021-07-05 17:07:39 -0500

    05-07

--- src/app/pages/agendamiento/asignacion-citas/otro-concepto/otro-concepto.component.ts
+++ src/app/pages/agendamiento/asignacion-citas/otro-concepto/otro-concepto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-otro-concepto',
@@ -10,6 +11,11 @@ export class OtroConceptoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  save(form:NgForm){
+    console.log(form);
+    
   }
 
 }
