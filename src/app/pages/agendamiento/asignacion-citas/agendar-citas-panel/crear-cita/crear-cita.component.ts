78a9c591185f3fcf41b43bb031560100873b99dd
daniloCorvus <danilo.grisalez@corvuslab.co>
2021-07-05 13:57:36 -0500

    salvando cita asignada

--- src/app/pages/agendamiento/asignacion-citas/agendar-citas-panel/crear-cita/crear-cita.component.ts
+++ src/app/pages/agendamiento/asignacion-citas/agendar-citas-panel/crear-cita/crear-cita.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 import { ips_remisors, professionals_remisors, especiality_remisors, procedurs_remisors } from './dataBurn';
 import { QueryPatient } from '../../../query-patient.service';
 import { formaterInput } from '../../../../../formaterInput'
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -25,14 +26,21 @@ export class CrearCitaComponent implements OnInit {
   public especiality_remisors
   public procedurs_remisors
   public space
+  public call
 
   diagnosticoId: any;
   procedureId: any;
 
-  searching = false;
-  searchFailed = false;
+  searchingDiagnostic = false;
+  searchingProcedure = false;
+  searchFailedDiagnostic = false;
+  searchFailedProcedure = false;
 
-  constructor(private _openAgendaService: OpenAgendaService, private _queryPatient: QueryPatient) { }
+  constructor(private _openAgendaService: OpenAgendaService, private _queryPatient: QueryPatient) {
+    this.call = JSON.parse(localStorage.getItem('patient'));
+
+  }
+
   typesDocuments: Array<any> = [
     { Nombre: 'CI', Id: '1' },
     { Nombre: 'CC', Id: '2' },
@@ -48,31 +56,54 @@ export class CrearCitaComponent implements OnInit {
     this.professionals_remisors = professionals_remisors
     this.especiality_remisors = especiality_remisors
     this.procedurs_remisors = procedurs_remisors
-    this._queryPatient.space.subscribe(r => this.space = r);
+
+    this._queryPatient.space.subscribe(r => {
+      this.space = r
+    });
+
   }
 
   save(form: NgForm) {
-    this._openAgendaService.saveCita(JSON.stringify(form.value))
-      .subscribe((data) => {
-        this._queryPatient.existPatient.emit('');
-      });
-    this.siguiente.emit('');
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success mx-2',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+    swalWithBootstrapButtons.fire({
+      title: '¿está seguro?',
+      text: "Se dispone a asignar una cita",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, ¡Hazlo !',
+      cancelButtonText: 'No, ¡dejeme comprobar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this._openAgendaService.saveCita(JSON.stringify(form.value))
+          .subscribe((data) => {
+            this._queryPatient.existPatient.emit('');
+          });
+        this.siguiente.emit('');
+      }
+    })
   }
 
   search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
     text$.pipe(
       debounceTime(300),
       distinctUntilChanged(),
-      tap(() => this.searching = true),
+      tap(() => this.searchingDiagnostic = true),
       switchMap(term => term.length < 3 ? [] :
         this._openAgendaService.search(term).pipe(
-          tap(() => this.searchFailed = false),
+          tap(() => this.searchFailedDiagnostic = false),
           catchError(() => {
-            this.searchFailed = true;
+            this.searchFailedDiagnostic = true;
             return of([]);
           }))
       ),
-      tap(() => this.searching = false)
+      tap(() => this.searchingDiagnostic = false)
     )
 
 
@@ -82,16 +113,16 @@ export class CrearCitaComponent implements OnInit {
     text$.pipe(
       debounceTime(300),
       distinctUntilChanged(),
-      tap(() => this.searching = true),
+      tap(() => this.searchingProcedure = true),
       switchMap(term => term.length < 3 ? [] :
         this._openAgendaService.searchProcedure(term).pipe(
-          tap(() => this.searchFailed = false),
+          tap(() => this.searchFailedProcedure = false),
           catchError(() => {
-            this.searchFailed = true;
+            this.searchFailedProcedure = true;
             return of([]);
           }))
       ),
-      tap(() => this.searching = false)
+      tap(() => this.searchingProcedure = false)
     )
 
   InputProcedure = (x: { text: string }) => x.text;
