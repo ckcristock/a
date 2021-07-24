57980848933483099c0b028d57a784aecb013e36
daniloCorvus <danilo.grisalez@corvuslab.co>
2021-07-24 13:33:22 -0500

    firts commit 24 julio

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -22,8 +22,8 @@
 export const environment = {
   production: false,
   
-  //  base_url: 'https://ateneo.test/api'
-   base_url: 'https://backend.ateneoerp.com/api'
+   base_url: 'https://ateneo.test/api'
+  //  base_url: 'https://backend.ateneoerp.com/api'
 
 };
 
