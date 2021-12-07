ae892806cfdbf13f0d29a8197d43d02d1ac50789
NestorCorvus <88901879+NestorCorvus@users.noreply.github.com>
2021-12-07 13:47:44 -0500

    se realizo la el cambio de variables en los planes de cuentas

--- src/app/pages/contabilidad/plan-cuentas/plan-cuentas.component.ts
+++ src/app/pages/contabilidad/plan-cuentas/plan-cuentas.component.ts
@@ -5,6 +5,10 @@ import { consts } from '../../../core/utils/consts';
 import { contabilidad } from "../contabilidad";
 import { ValidatorsService } from '../../ajustes/informacion-base/services/reactive-validation/validators.service';
 import { PlanCuentasService } from './plan-cuentas.service';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-plan-cuentas',
@@ -25,25 +29,34 @@ export class PlanCuentasComponent implements OnInit {
   accountTypes = contabilidad.accountType;
   cuenta = contabilidad.account;
   status = contabilidad.statusAccounts;
-  pagination:any = {
+  pagination = {
     page: 1,
     pageSize: 5,
     collectionSize: 0
   }
   filtros:any = {
-    code: '',
-    name: '',
-    niif_code: '',
-    niff_name: '',
-    status: ''
+    filtro_codigo: '',
+    filtro_codigo_niif: '',
+    filtro_nombre: '',
+    filtro_nombre_niif: '',
+    filtro_estado_cuenta: ''
+  }
+  public InformacionPaginacion:any = {
+    desde: 0,
+    hasta: 0,
+    total: 0
   }
   constructor( 
                 private fb: FormBuilder,
                 private _validators: ValidatorsService,
-                private _planAccountService: PlanCuentasService ) { }
+                private _planAccountService: PlanCuentasService,
+                private location: Location,
+                private route: ActivatedRoute,
+                private http: HttpClient ) { }
 
   ngOnInit(): void {
     this.createForm();
+    this.RecargarDatos();
     this.getAccount_plan();
   }
 
@@ -60,35 +73,89 @@ export class PlanCuentasComponent implements OnInit {
   createForm(){
     this.form = this.fb.group({
       id: [this.account.id],
-      type_p: ['',this._validators.required],
-      type_niif: ['',this._validators.required],
-      code: ['',this._validators.required],
-      name: ['',this._validators.required],
-      niif_code: ['',this._validators.required],
-      niif_name: ['',this._validators.required],
-      accounting_adjustment: [''],
-      close_third: [''],
-      movement: [0],
-      document: [''],
-      base: [''],
-      value: [''],
-      percent: [''],
-      center_cost: [''],
-      depreciation: [''],
-      amortization: [''],
-      exogenous: [''],
-      nature: ['',this._validators.required],
-      close_nit: [''],
-      bank: [''],
-      bank_id: [''],
-      nit: [''],
-      report: [''],
-      class_account: [''],
-      niif: ['',this._validators.required],
-      annual_voucher: [''],
-      status: ['', this._validators.required],
-      account_number: ['', this._validators.required],
+      Tipo_P: this,
+      Tipo_Niif: this,
+      Codigo: this,
+      Nombre: ['',this._validators.required],
+      Codigo_Niif: ['',this._validators.required],
+      Nombre_Niif: ['',this._validators.required],
+      Ajuste_Contable: [''],
+      Cierra_Terceros: [''],
+      Movimiento: [0],
+      Documento: [''],
+      Base: [''],
+      Valor: [''],
+      Porcentaje: [''],
+      Centro_Costo: [''],
+      Depreciacion: [''],
+      Amortizacion: [''],
+      Exogeno: [''],
+      Naturaleza: ['',this._validators.required],
+      Nit_Cierre: [''],
+      Banco: [''],
+      Cod_Banco: [''],
+      Nit: [''],
+      Reporte: [''],
+      Clase_Cta: [''],
+      Niif: [''],
+      Cie_Anual: [''],
+      Estado: ['', this._validators.required],
+      Cta_Numero: ['', this._validators.required],
     });
+  }
+
+  SetFiltros(paginacion:boolean = false){
+    let params:any = {};
+
+    if( paginacion === true ){      
+      params.pag = this.pagination.page;
+    } else {      
+      this.pagination.page = 1;
+      params.pag = this.pagination.page;
+    }
+    
+    if (this.filtros.filtro_codigo != "") {
+      params.cod= this.filtros.filtro_codigo;
+    }
+    if (this.filtros.filtro_codigo_niif != "") {
+      params.cod_niif = this.filtros.filtro_codigo_niif;
+    }
+    if (this.filtros.filtro_nombre != "") {
+      params.nombre = this.filtros.filtro_nombre;
+    }
+    if (this.filtros.filtro_nombre_niif != "") {
+      params.nombre_niif = this.filtros.filtro_nombre_niif;
+    }
+    if (this.filtros.filtro_estado_cuenta != "") {
+      params.estado = this.filtros.filtro_estado_cuenta;
+    }
+
+    let queryString = '?' + Object.keys(params).map(key => key + '=' + params[key]).join('&');
+    return queryString;
+  }
+
+  SetInformacionPaginacion(){
+    var calculoHasta = (this.pagination.page*this.pagination.pageSize);
+    var desde = calculoHasta - this.pagination.pageSize+1;
+    var hasta = calculoHasta > this.pagination.collectionSize ? this.pagination.collectionSize : calculoHasta;
+
+    this.InformacionPaginacion['desde'] = desde;
+    this.InformacionPaginacion['hasta'] = hasta;
+    this.InformacionPaginacion['total'] = this.pagination.collectionSize;
+  }
+
+  filtross(paginacion:boolean = false) {
+    this.loading = true;
+    var params = this.SetFiltros(paginacion);
+
+    this.location.replaceState('plan-cuentas', params);    
+
+    this.http.get(environment.ruta + '/php/plancuentas/lista_plan_cuentas.php' + params).subscribe((res:any) => {
+      this.account = res.query_result;
+      this.pagination.collectionSize = res.numReg;
+      this.SetInformacionPaginacion();
+      this.loading = false;
+    })
   }
 
   getAccount_plan( page = 1 ) {
@@ -97,12 +164,32 @@ export class PlanCuentasComponent implements OnInit {
       ...this.pagination, ...this.filtros
     }
     this.loading = true;
-    this._planAccountService.getAccount_plan(params)
-    .subscribe( (res:any) => {
-      this.accounts = res.data.data;
-      this.pagination.collectionSize = res.data.total;
+    this.http.get(environment.ruta + '/php/plancuentas/lista_plan_cuentas.php').subscribe((res:any) => {
+      this.accounts = res.query_result;
+      console.log(this.account);
+      // this.pagination.collectionSize = res.numReg;
       this.loading = false;
     })
+  }
+
+  AsignarParametrosUrl(urlParams:any){
+    this.pagination.page = urlParams.pag ? urlParams.pag : 1;
+    this.filtros.filtro_codigo_niif = urlParams.cod_niif ? urlParams.cod_niif : '';
+    this.filtros.filtro_codigo = urlParams.cod ? urlParams.cod : '';
+    this.filtros.filtro_nombre = urlParams.nombre ? urlParams.nombre : '';
+    this.filtros.filtro_nombre_niif = urlParams.nombre_niif ? urlParams.nombre_niif : '';
+    this.filtros.filtro_estado_cuenta = urlParams.estado ? urlParams.estado : '';
+  }
+
+  RecargarDatos() {
+    let urlParams = this.route.snapshot.queryParams;
+    if (Object.keys(urlParams).length > 0) { // Si existe parametros o filtros
+      // actualizando la variables con los valores de los paremetros.
+      this.AsignarParametrosUrl(urlParams);
+      this.filtross(this.pagination.page > 1);
+    }else{
+      this.filtross();
+    }
   }
   
   getAccount(account) {
@@ -111,89 +198,86 @@ export class PlanCuentasComponent implements OnInit {
     this.title = 'Actualizar Cuenta'
     this.form.patchValue({
       id: this.account.id,
-      type_p: this.account.type_p,
-      type_niif: this.account.type_niif,
-      code: this.account.code,
-      name: this.account.name,
-      niif_code: this.account.niif_code,
-      niif_name: this.account.niif_name,
-      accounting_adjustment: this.account.accounting_adjustment,
-      close_third: this.account.close_third,
-      movement: this.account.movement,
-      document: this.account.document,
-      base: this.account.base,
-      value: this.account.value,
-      percent: this.account.percent,
-      center_cost: this.account.center_cost,
-      depreciation: this.account.depreciation,
-      amortization: this.account.amortization,
-      exogenous: this.account.exogenous,
-      nature: this.account.nature,
-      close_nit: this.account.close_nit,
-      bank: this.account.bank,
-      bank_id: this.account.bank_id,
-      nit: this.account.nit,
-      report: this.account.report,
-      class_account: this.account.class_account,
-      niif: this.account.niif,
-      annual_voucher: this.account.annual_voucher,
-      status: this.account.status,
-      account_number: this.account.account_number,
+      Tipo_P: this.account.Tipo_P,
+      Tipo_Niif: this.account.Tipo_Niif,
+      Codigo: this.account.Codigo,
+      Nombre: this.account.Nombre,
+      Codigo_Niif: this.account.Codigo_Niif,
+      Nombre_Niif: this.account.Nombre_Niif,
+      Ajuste_Contable: this.account.Ajuste_Contable,
+      Cierra_Terceros: this.account.Cierra_Terceros,
+      Movimiento: this.account.Movimiento,
+      Documento: this.account.Documento,
+      Base: this.account.Base,
+      Valor: this.account.Valor,
+      Porcentaje: this.account.Porcentaje,
+      Centro_Costo: this.account.Centro_Costo,
+      Depreciacion: this.account.Depreciacion,
+      Amortizacion: this.account.Amortizacion,
+      Exogeno: this.account.Exogeno,
+      Naturaleza: this.account.Naturaleza,
+      Nit_Cierre: this.account.Nit_Cierre,
+      Banco: this.account.Banco,
+      Cod_Banco: this.account.Cod_Banco,
+      Nit: this.account.Nit,
+      Reporte: this.account.Reporte,
+      Clase_Cta: this.account.Clase_Cta,
+      Cie_Anual: this.account.Cie_Anual,
+      Estado: this.account.Estado,
+      Cta_Numero: this.account.Cta_Numero,
     });
   }
 
   enableFields(fild) {
     if (fild == 1) {
-      this.form.get('base').disable();
-      this.form.get('value').disable();
-      this.form.get('percent').disable();
-      this.form.get('accounting_adjustment').disable();
-      this.form.get('annual_voucher').disable();
-      this.form.get('close_third').disable();
-      this.form.get('close_nit').disable();
-      this.form.get('bank').disable();
-      this.form.get('bank_id').disable();
-      this.form.get('nit').disable();
-      this.form.get('class_account').disable();
-      this.form.get('document').disable();
-      this.form.get('account_number').disable();
-      this.form.get('report').disable();
-      this.form.get('exogenous').disable();
-      this.form.get('center_cost').disable();
+      this.form.get('Base').disable();
+      this.form.get('Valor').disable();
+      this.form.get('Porcentaje').disable();
+      this.form.get('Ajuste_Contable').disable();
+      this.form.get('Cie_Anual').disable();
+      this.form.get('Cierra_Terceros').disable();
+      this.form.get('Nit_Cierre').disable();
+      this.form.get('Banco').disable();
+      this.form.get('Cod_Banco').disable();
+      this.form.get('Nit').disable();
+      this.form.get('Clase_Cta').disable();
+      this.form.get('Documento').disable();
+      this.form.get('Cta_Numero').disable();
+      this.form.get('Reporte').disable();
+      this.form.get('Exogeno').disable();
+      this.form.get('Centro_Costo').disable();
     } else {
-      this.form.get('base').enable();
-      this.form.get('value').enable();
-      this.form.get('percent').enable();
-      this.form.get('accounting_adjustment').enable();
-      this.form.get('annual_voucher').enable();
-      this.form.get('close_third').enable();
-      this.form.get('close_nit').enable();
-      this.form.get('bank').enable();
-      this.form.get('bank_id').enable();
-      this.form.get('nit').enable();
-      this.form.get('class_account').enable();
-      this.form.get('document').enable();
-      this.form.get('status').enable();
-      this.form.get('account_number').enable();
-      this.form.get('report').enable();
-      this.form.get('exogenous').enable();
-      this.form.get('center_cost').enable();
+      this.form.get('Base').enable();
+      this.form.get('Valor').enable();
+      this.form.get('Porcentaje').enable();
+      this.form.get('Ajuste_Contable').enable();
+      this.form.get('Cie_Anual').enable();
+      this.form.get('Cierra_Terceros').enable();
+      this.form.get('Nit_Cierre').enable();
+      this.form.get('Banco').enable();
+      this.form.get('Cod_Banco').enable();
+      this.form.get('Nit').enable();
+      this.form.get('Clase_Cta').enable();
+      this.form.get('Documento').enable();
+      this.form.get('Estado').enable();
+      this.form.get('Cta_Numero').enable();
+      this.form.get('Reporte').enable();
+      this.form.get('Exogeno').enable();
+      this.form.get('Centro_Costo').enable();
     }
   }
 
   createAccount_plan() {
-    /* this.form.markAllAsTouched()
-        if (this.form.invalid) { return false } */
-    this._planAccountService.createAccount_plan(this.form.value)
-    .subscribe( (res:any) => {
-        this.modal.hide();
-        this.getAccount_plan();
-        this.form.reset();
-        Swal.fire({
-          icon: 'success',
-          title: res.data
-        });
-    })
+    this.http.post(`${environment.ruta}/php/contabilidad/plancuentas/guardar_puc.php`, this.form.value)
+    .subscribe((r:any) => {
+      this.modal.hide();
+      this.getAccount_plan();
+      this.form.reset();
+      Swal.fire({
+        icon: 'success',
+        title: 'Plan de cuenta creado con éxito'
+      });
+    });
   }
 
   activateOrInactivate(contract, status) {
@@ -228,7 +312,7 @@ export class PlanCuentasComponent implements OnInit {
 
   getBanks(r) {
     if (r == 0) {
-      this.form.get('bank_id').enable();
+      this.form.get('Cod_Banco').enable();
       this.bankSelected = true;
       this._planAccountService.getBanks()
       .subscribe( (res:any) => {
@@ -240,47 +324,47 @@ export class PlanCuentasComponent implements OnInit {
   }
 
   get movement_invalid() {
-    return this.form.get('movement').invalid && this.form.get('movement').touched;
+    return this.form.get('Movimiento').invalid && this.form.get('Movimiento').touched;
   }
 
   get code_invalid() {
-    return this.form.get('code').invalid && this.form.get('code').touched;
+    return this.form.get('Codigo').invalid && this.form.get('Codigo').touched;
   }
 
   get type_p_invalid() {
-    return this.form.get('type_p').invalid && this.form.get('type_p').touched;
+    return this.form.get('Tipo_P').invalid && this.form.get('Tipo_P').touched;
   }
 
   get type_niif_invalid() {
-    return this.form.get('type_niif').invalid && this.form.get('type_niif').touched;
+    return this.form.get('Tipo_Niif').invalid && this.form.get('Tipo_Niif').touched;
   }
 
   get name_invalid() {
-    return this.form.get('name').invalid && this.form.get('name').touched;
+    return this.form.get('Nombre').invalid && this.form.get('Nombre').touched;
   }
 
   get niif_code_invalid() {
-    return this.form.get('niif_code').invalid && this.form.get('niif_code').touched;
+    return this.form.get('Codigo_Niif').invalid && this.form.get('Codigo_Niif').touched;
   }
 
   get niif_name_invalid() {
-    return this.form.get('niif_name').invalid && this.form.get('niif_name').touched;
+    return this.form.get('Nombre_Niif').invalid && this.form.get('Nombre_Niif').touched;
   }
 
   get niif_invalid() {
-    return this.form.get('niif').invalid && this.form.get('niif').touched;
+    return this.form.get('Niif').invalid && this.form.get('Niif').touched;
   }
 
   get account_number_invalid() {
-    return this.form.get('account_number').invalid && this.form.get('account_number').touched;
+    return this.form.get('Cta_Numero').invalid && this.form.get('Cta_Numero').touched;
   }
 
   get status_invalid() {
-    return this.form.get('status').invalid && this.form.get('status').touched;
+    return this.form.get('Estado').invalid && this.form.get('Estado').touched;
   }
 
   get nature_invalid() {
-    return this.form.get('nature').invalid && this.form.get('nature').touched;
+    return this.form.get('Naturaleza').invalid && this.form.get('Naturaleza').touched;
   }
 
 }
--- src/app/pages/contabilidad/plan-cuentas/plan-cuentas.service.ts
+++ src/app/pages/contabilidad/plan-cuentas/plan-cuentas.service.ts
@@ -10,11 +10,15 @@ export class PlanCuentasService {
   constructor( private http: HttpClient ) { }
 
   getAccount_plan( params = {} ) {
-    return this.http.get(`${environment.base_url}/account_plan`, {params});
+    return this.http.get(`${environment.ruta}/php/plancuentas/lista_plan_cuentas.php`, {params});
   }
     
   createAccount_plan( data:any ) {
-    return this.http.post(`${environment.base_url}/account_plan`, data);
+    return this.http.post(`${environment.ruta}/php/contabilidad/plancuentas/guardar_puc.php`, data);
+  }
+
+  editAccounPplan( data:any ) {
+    return this.http.post(`${environment.ruta}/php/contabilidad/plancuentas/detalle_plan_cuenta.php`, data);
   }
 
   getBanks() {
