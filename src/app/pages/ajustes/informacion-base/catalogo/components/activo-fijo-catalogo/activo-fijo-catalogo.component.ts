19c925d444bda915ca8fa393c9004f82937b271a
Roberth Morales <79588731+roberthCorvus@users.noreply.github.com>
2021-12-27 15:26:48 -0500

    catalogo
    se crea base para catalogos

---
+++ src/app/pages/ajustes/informacion-base/catalogo/components/activo-fijo-catalogo/activo-fijo-catalogo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activo-fijo-catalogo',
+  templateUrl: './activo-fijo-catalogo.component.html',
+  styleUrls: ['./activo-fijo-catalogo.component.scss']
+})
+export class ActivoFijoCatalogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
