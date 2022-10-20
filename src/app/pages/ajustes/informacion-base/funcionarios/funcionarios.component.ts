1ac43b4ed2ad357a3817740d71b1489e99256cc6
brayandavilao <100726985+brayandavilao@users.noreply.github.com>
2022-10-20 14:45:58 -0500

    A1-41

--- src/app/pages/ajustes/informacion-base/funcionarios/funcionarios.component.ts
+++ src/app/pages/ajustes/informacion-base/funcionarios/funcionarios.component.ts
@@ -7,70 +7,32 @@ import { DependenciesService } from '../services/dependencies.service';
 import { CompanyService } from '../services/company.service';
 import { MatAccordion } from '@angular/material/expansion';
 
-
 @Component({
   selector: 'app-funcionarios',
   templateUrl: './funcionarios.component.html',
-  styleUrls: ['./funcionarios.component.scss']
+  styleUrls: ['./funcionarios.component.scss'],
 })
-
 export class FuncionariosComponent implements OnInit {
   @ViewChild(MatAccordion) accordion: MatAccordion;
   pagination = {
     pageSize: 12,
     page: 1,
     collectionSize: 0,
-  }
-  loading = false;
+  };
+  loading: boolean;
   breadCrumbItems: Array<{}>;
   people: Person[] = [];
 
   status: any[] = [
-    { id: 1, name: 'Activo', selected: true },
-    { id: 2, name: 'Suspendido', selected: true },
-    { id: 3, name: 'Liquidado', selected: true },
+    { id: 1, name: 'Activo', selected: true, color: 'text-success' },
+    { id: 2, name: 'Inactivo', selected: true, color: 'text-danger' },
+    { id: 3, name: 'Liquidado', selected: true, color: 'text-secondary' },
+    { id: 4, name: 'PreLiquidado', selected: true, color: 'text-warning' },
   ];
 
-  public dependencies: any = []
+  public dependencies: any = [];
 
-  public companies: any = [
-    {
-      id: 1,
-      name: "HEMOPLIFE SALUD"
-    },
-    {
-      id: 2,
-      name: "MEGSALUD IPS"
-    },
-    {
-      id: 3,
-      name: "ECOMEDIS"
-    },
-    {
-      id: 4,
-      name: "VIDASER"
-    },
-    {
-      id: 5,
-      name: "MEDISERRANO"
-    },
-    {
-      id: 6,
-      name: "SALUD VITAL"
-    },
-    {
-      id: 7,
-      name: "MASCORP"
-    },
-    {
-      id: 2,
-      name: "INGBUS"
-    },
-    {
-      id: 2,
-      name: "INNOVATING"
-    }
-  ]
+  public companies: any[] = [];
   isCollapsed: boolean;
   collapsed: boolean;
   collapsed3: boolean;
@@ -79,18 +41,20 @@ export class FuncionariosComponent implements OnInit {
   //   this.getPeople();
   // }
 
-  constructor(
+  constructor (
     private _person: PersonService,
     private _dependencies: DependenciesService,
-    private _companies: CompanyService) {
-
-  }
+    private _companies: CompanyService
+  ) {}
 
   ngOnInit(): void {
     this.getDependencies();
     this.getCompanies();
 
-    this.breadCrumbItems = [{ label: 'Ecommerce' }, { label: 'Product', active: true }];
+    this.breadCrumbItems = [
+      { label: 'Ecommerce' },
+      { label: 'Product', active: true },
+    ];
     this.isCollapsed = false;
     this.collapsed = false;
     this.collapsed3 = false;
@@ -98,72 +62,62 @@ export class FuncionariosComponent implements OnInit {
 
   getDependencies() {
     this._dependencies.getDependencies().subscribe((r: any) => {
-      this.dependencies = r.data
-      this.dependencies = this.dependencies.map(r => {
+      this.dependencies = r.data;
+      this.dependencies = this.dependencies.map((r) => {
         r.selected = true;
-        return r
-      })
+        return r;
+      });
       this.getPeople();
-    })
+    });
   }
 
   getCompanies() {
-    let params = { 'owner': 1 }
+    let params = { owner: 1 };
     this._companies.getCompanies(params).subscribe((r: any) => {
-      this.companies = r.data
-      this.companies = this.companies.map(r => {
+      this.companies = r.data;
+      this.companies = this.companies.map((r) => {
         r.selected = true;
-        return r
-      })
+        return r;
+      });
       this.getPeople();
-    })
+    });
   }
 
-
-
-  //   this.pagination.page = page;
-  //   let params: any = { ...this.pagination }
-  //   console.log(params);
-  //   params.status = this.statusFilter();
-  //   params.dependencies = this.dependenciesFilter();
-  //   params.name = name ? name : ''
-  // console.log(params);
-
-  //   this.loading = true;
-
-  //   this._person.getPeople({data:JSON.stringify( params )})
-  //     .subscribe(d => {
-  //       this.loading = false;
-  //       this.people = d['data']['data']
-  //       this.pagination.collectionSize = d['data']['total']
-  //     })
-
   getPeople(page = 1, name = '') {
-
     this.pagination.page = page;
-    let params: any = { ...this.pagination }
+    let params: any = { ...this.pagination };
     params.status = this.statusFilter();
     params.dependencies = this.dependenciesFilter();
     params.companies = this.companiesFilter();
-    params.name = name ? name : ''
+    params.name = name ? name : '';
 
     this.loading = true;
-    this._person.getPeople({ data: JSON.stringify(params) })
-      .subscribe(d => {
-        this.loading = false;
-        this.people = d['data']['data']
-        this.pagination.collectionSize = d['data']['total']
-      })
+    this._person.getPeople({ data: JSON.stringify(params) }).subscribe((d) => {
+      this.people = d['data']['data'];
+      this.pagination.collectionSize = d['data']['total'];
+      this.loading = false;
+    });
   }
-
 
   statusFilter() {
-    return this.status.reduce((acc: Array<any>, el) => el.selected == true ? acc.concat([el.name]) : acc, [])
+    return this.status.reduce(
+      (acc: Array<any>, el) =>
+        el.selected == true ? acc.concat([el.name]) : acc,
+      []
+    );
   }
   dependenciesFilter() {
-    return this.dependencies.reduce((acc: Array<any>, el) => el.selected == true ? acc.concat([el.value]) : acc, [])
+    return this.dependencies.reduce(
+      (acc: Array<any>, el) =>
+        el.selected == true ? acc.concat([el.value]) : acc,
+      []
+    );
   }
   companiesFilter() {
-    return this.companies.reduce((acc: Array<any>, el) => el.selected == true ? acc.concat([el.value]) : acc, [])
+    return this.companies.reduce(
+      (acc: Array<any>, el) =>
+        el.selected == true ? acc.concat([el.value]) : acc,
+      []
+    );
   }
 }
