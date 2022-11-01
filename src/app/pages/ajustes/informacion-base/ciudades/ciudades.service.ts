c732d5073b243889dfbf96ce7bd674ebf2ae6371
CesarFlorezCorvusLab <cesar.florez@corvuslab.co>
2022-11-01 18:07:24 -0500

    Modificaciones Varias

--- src/app/pages/ajustes/informacion-base/ciudades/ciudades.service.ts
+++ src/app/pages/ajustes/informacion-base/ciudades/ciudades.service.ts
@@ -8,13 +8,21 @@ import { environment } from 'src/environments/environment';
 export class CiudadesService {
 
   constructor( private http: HttpClient ) { }
-  
+
   getContries(){
     return this.http.get(`${environment.base_url}/countries`);
   }
 
   createCity( data:any ){
     return this.http.post(`${environment.base_url}/cities`, data);
+  }
+
+  getCitiesByStateId(state_id, params){ //ciudades por Departamento id
+    return this.http.get(`${environment.base_url}/cities/${state_id}`, {params});
+  }
+
+  getCitiesByMunicipalityId(municipality_id, params){
+    return this.http.get(`${environment.base_url}/cities-by-municipalities/${municipality_id}`, {params});
   }
 
   getCities( params = {} ){
